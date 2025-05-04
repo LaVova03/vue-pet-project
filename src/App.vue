6fe3b77df666cb7b6748e7e771d601d10d12a19c
volodymyr <davidvovchik@gmail.com>
2025-05-04 10:18:15 +0300

    add new project

--- src/App.vue
+++ src/App.vue
@@ -1,122 +1,21 @@
 <template>
-  <div class="app">
-    <header>
-      <nav>
-        <router-link class="nav" to="/">Home</router-link> |
-        <router-link
-          class="nav"
-          to="/main"
-          :class="{ 'disabled-link': !token }"
-          :style="{ pointerEvents: token ? 'auto' : 'none' }"
-          >Main
-        </router-link>
-      </nav>
-      <div v-if="token">
-        <MainButton @click="logOut">LogOut</MainButton>
-      </div>
-      <div v-show="route == '/main'">
-        <MainButton @click="createCard">Create</MainButton>
-      </div>
-    </header>
-    <main>
-      <router-view v-slot="{ Component }">
-        <keep-alive>
-          <component :is="Component" />
-        </keep-alive>
-      </router-view>
-    </main>
-    <footer>
-      <SvgSprite symbol="one" size="30" fill="red" />
-      <SvgSprite symbol="cloud" size="30" fill="red" />
-      <SvgSprite symbol="address" size="30" fill="red" />
-      <SvgSprite symbol="vite" size="30" />
-    </footer>
-    <CardModal
-      v-show="showCreateModal"
-      @open-edit-modal="showCreateModal = !showCreateModal"
-    >
-      <h2>Create Card</h2>
-    </CardModal>
+  <div class="container">
+    <header>header</header>
+    <main>main</main>
+    <footer>footer</footer>
   </div>
 </template>
 
-<script>
-import { mapActions } from "vuex";
-import { SvgSprite } from "vue-svg-sprite";
-import MainButton from "@/components/MainButton.vue";
-import CardModal from "./components/CardModal.vue";
-
-export default {
-  components: {
-    SvgSprite,
-    MainButton,
-    CardModal,
-  },
-  data() {
-    return {
-      showCreateModal: false,
-    };
-  },
-  computed: {
-    token() {
-      return this.$store.state.token;
-    },
-    route() {
-      return this.$route.path;
-    },
-  },
-  methods: {
-    ...mapActions(["setToken"]),
-    logOut() {
-      this.$store.commit("setToken", "");
-      sessionStorage.clear();
-      this.$router.push("/");
-    },
-    createCard() {
-      this.showCreateModal = !this.showCreateModal;
-    },
-  },
-};
-</script>
+<script></script>
 
 <style lang="less">
 @import "@/assets/global.less";
 
-.app {
-  width: calc(100vw - 8rem);
+.container {
   position: relative;
-  min-height: calc(100vh - 4rem);
-  padding: 2rem;
+  min-height: 100vh;
+  width: 100vw;
+  background-color: @bkg-color;
   .column();
-
-  header div {
-    position: absolute;
-    top: 1rem;
-    right: 0;
-  }
-
-  header > div:nth-of-type(2) {
-    width: max-content;
-    right: none;
-    left: 0;
-  }
-
-  footer {
-    display: flex;
-    gap: 10px;
-  }
-}
-
-.disabled-link {
-  color: grey;
-  pointer-events: none;
-}
-
-@media (max-width: 768px) {
-  .app {
-    width: calc(100vw - 5rem);
-    padding: 4rem 0 1rem;
-    min-height: calc(100vh - 5rem);
-  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
-import store from './store';
-import router from './router';
 import { svgSpritePlugin } from 'vue-svg-sprite';
 
 createApp(App)
-    .use(store)
-    .use(router)
     .use(svgSpritePlugin, {
         url: 'icons/sprite.svg',
         class: 'icon',
